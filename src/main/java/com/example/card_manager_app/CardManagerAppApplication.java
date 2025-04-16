5bfa2cf8dcafe12216dc55f942c09cd6a1ac5877
Constantine <Rikkki.Tikkki.Tavi@yandex.ru>
2025-04-16 12:57:04 +0300

    init commit

---
+++ src/main/java/com/example/card_manager_app/CardManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.card_manager_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardManagerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/card_manager_app/CardManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.card_manager_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
