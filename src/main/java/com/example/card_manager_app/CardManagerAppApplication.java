0fcfb9f70cf698cc6607da17ab472df6ea58ad1a
Constantine <Rikkki.Tikkki.Tavi@yandex.ru>
2025-04-17 22:40:35 +0300

    feat: add docker compose

--- src/main/java/com/example/card_manager_app/CardManagerAppApplication.java
+++ src/main/java/com/example/card_manager_app/CardManagerAppApplication.java
@@ -2,8 +2,16 @@ package com.example.card_manager_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EntityScan(basePackages = "com.example")
+@ComponentScan(basePackages = "com.example")
+@EnableJpaRepositories(basePackages = "com.example.*")
 public class CardManagerAppApplication {
 
 	public static void main(String[] args) {
